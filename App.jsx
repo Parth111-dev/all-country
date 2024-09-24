51d8b886d45e6178f9fdc0e7510ef1d8b4fd81bb
Parth111-dev <parthpatel685075@gmail.com>
2024-09-24 15:41:51 +0530

    theme problem solved

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import Header from './Components/Header'
 import './App.css'
 import { Outlet } from 'react-router-dom'
-import ThemWrapper from './Context/themeContext'
+import ThemWrapper from './Context/ThemeContext'
 
 const App = () => {
   return (
--- Hooks/useTheme.js
+++ Hooks/useTheme.js
@@ -1,4 +1,4 @@
 import { useContext } from "react";
-import { ThemeContext } from "../Context/themeContext";
+import { ThemeContext } from "../Context/ThemeContext";
 
 export const useTheme = () => useContext(ThemeContext)
